28b262c8730579d6c8bfe7ed52e589805f45b89c
Solurio <113849792+Solurio@users.noreply.github.com>
2025-10-31 21:49:13 -0300

    Comment Paciente.h

--- trabalho/Paciente.h
+++ trabalho/Paciente.h
@@ -1,37 +1,48 @@
 #ifndef PACIENTE_H
 #define PACIENTE_H
 
+// Paciente.h
+// Classe concreta: Paciente
+// Atributos: id, diagnostico.
+
 #include "Pessoa.h"
+#include <string>
+#include <iostream>
 
 class Paciente : public Pessoa {
 private:
     long int id;
-    string diagnostico;
+    std::string diagnostico;
 
 public:
+    // Construtor
     Paciente() : id(0), diagnostico("não identificado") {
         setnome("no-name");
         setidade(0);
     }
 
-    Paciente(long int id, const string& nome, short int idade, const string& diag) {
+    // Construtor com parâmetros
+    Paciente(long int id, const std::string& nome, short int idade, const std::string& diag) {
         setid(id);
         setnome(nome);
         setidade(idade);
         setdiagnostico(diag);
-        cout << "Paciente: " << getnome() << " registrado..." << endl;
+        std::cout << "Paciente: " << getnome() << " registrado..." << std::endl;
     }
 
+    // Get/Set do id
     void setid(long int id) { this->id = id; }
     long int getid() const { return id; }
 
-    void setdiagnostico(const string& diag) { diagnostico = diag; }
-    string getdiagnostico() const { return diagnostico; }
+    // Get/Set do diagnóstico
+    void setdiagnostico(const std::string& diag) { diagnostico = diag; }
+    std::string getdiagnostico() const { return diagnostico; }
 
+    // Implementando o método polimórfico
     void display() const override {
-        cout << "Paciente [" << getid() << "]: " << getnome()
-             << ", Idade: " << getidade()
-             << ", Diagnóstico: " << getdiagnostico() << endl;
+        std::cout << "Paciente [" << getid() << "]: " << getnome()
+                  << ", Idade: " << getidade()
+                  << ", Diagnóstico: " << getdiagnostico() << std::endl;
     }
 };
 
