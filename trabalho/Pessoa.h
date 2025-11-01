53ce06bf692013c8438f62b57d9e8ee7d784cc8c
Solurio <113849792+Solurio@users.noreply.github.com>
2025-10-31 21:45:34 -0300

    Comment Pessoa.h

--- trabalho/Pessoa.h
+++ trabalho/Pessoa.h
@@ -1,32 +1,40 @@
 #ifndef PESSOA_H
 #define PESSOA_H
 
+// Pessoa.h
+// Define a classe abstrata Pessoa.
+// Serve de base para Paciente e Medico.
+
 #include <string>
 #include <iostream>
-using namespace std;
 
 class Pessoa {
 private:
-    string nome;
+    std::string nome;
     short int idade;
 
 public:
+    // Construtor
     Pessoa() : nome("no-name"), idade(0) {}
-    Pessoa(const string& nome, short int idade) {
+
+    // Construtor com parâmetros
+    Pessoa(const std::string& nome, short int idade) {
         setnome(nome);
         setidade(idade);
     }
+
+    // Destrutor virtual
     virtual ~Pessoa() {}
 
-    void setnome(const string& nome) { this->nome = nome; }
-    string getnome() const { return nome; }
+    // Encapsulamento get/set
+    void setnome(const std::string& nome) { this->nome = nome; }
+    std::string getnome() const { return nome; }
 
     void setidade(short int idade) { this->idade = idade; }
     short int getidade() const { return idade; }
 
+    // Método polimórfico
     virtual void display() const = 0;
 };
 
 #endif
-
-
